17be4af7a2df7463327045c14c96199988706e5c
EricNdivo <j.ericndivo@gmail.com>
2024-06-05 14:33:29 +0300

    reconfiguredtests

--- movie_recommendation/movie/tests.py
+++ movie_recommendation/movie/tests.py
@@ -1,99 +1,51 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token 
-from .models import Movie, Rating, Follow
+from rest_framework.test import APITestCase
+from .models import Movie, Rating, User
 
-class CustomAuthTokenTests(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.url = reverse('api-token')
-
-    def test_obtain_auth_token(self):
-        data = {'username': 'testuser', 'password': 'testpassword'}
-        response =  self.client.post(self.url, data, format='json')
-        self.assertEqual(response.status-code, status.HTTP_200_OK)
-        self.assertIn('token', response.data)
-
-class RegisterViewTests(APITestCase):
-    def setUp(self):
-        self.url = reverse('register')
-
-    def test_register_user(self):
-        data = {'username': 'newuser', 'password': 'newpassword', 'email': 'newuser@example.com'}
-        response = self.client.post(self.url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 1)
-
-class FetchNewMoviesViewTests(APITestCase):
-    def setUp(self):
-        self.url = reverse('fetch-new-movies')
-
-    def test_fetch_new_movies(self):
-        response = self.client.get(self.url)
+class FetchNewMoviesViewTestCase(APITestCase):
+    def test_fetch_new_movies_success(self):
+        url = reverse('fetch-new-movies')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('status', response.data)
-        self.assertIn('data', response.data)
-class MovieViewSetTests(APITestCase):
-    def setUp(self):
-        self.movie = Movie.objects.create(title='Test Movie', description='Test Description', release_date='2023-01-01')
-        self.url = reverse('movie-list')
-    def test_list_movies(self):
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-
-
-class RatingViewSetTests(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.movie = Movie.objects.create(title='Test Movie', description='Test Description', release_date='2023-01-01')
-        self.rating = Rating.objects.create(user=self.user, movie=self.movie, rating=5, review='Great movie')
-        self.client = APIClient()
-        self.client.force_authenticate(user=self.user)
-
-    def test_create_rating(self):
-        url = reverse('rating-list')
-        data = {'movie_id': self.movie.id, 'rating': 4, 'review': 'Good Movie'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Movie.objects.count(), len(response.data['data']))
     
 
-class RecommendationViewSetTests(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.client =APIClient()
-        self.client.force_authenticate(user=self.user)
-        self.url = reverse('recommendation-list', kwargs={'user_id' : self.user.id})
+    def test_fetch_new_movies_failed(self):
+        url = reverse('fetch-new-movies')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_get_recommendations(self):
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-
-class TopRatedMoviesViewSetTests(APITestCase):
-    def setUp(self):
-        self.movie = Movie.objects.create(title='Test Movie', description='Test Description', release_date='2023-01-01')
-        self.rating = Rating.objects.create(movie=self.movie, rating=5)
-        self.url = reverse('top-rated')
-
-    def test_top_rated_movies(self):
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-class ReviewViewSetTests(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.movie = Movie.objects.create(title='Test Movie', description='test description', release_year='2023-01-01')
-        self.rating = Rating.objects.create(user=self.user, movie=self.movie, rating=5, review='Great movie')
-        self.client = APIClient()
-        self.client.force_authenticate(user=self.user)
-        self.url = reverse('review-list')
-
-
-    def test_create_review(self):
-        data = {'movie_id': self.movie.id, 'rating': 4, 'review': 'Good movie'}
-        response = self.client.post(self.url, data, format='json')
+class RegisterViewTestCase(APITestCase):
+    def test_register_user_success(self):
+        url = reverse('register')
+        data = {'username': 'testuser', 'email': 'test@example.com', 'password': 'password'}
+        response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+class LoginViewTestCase(APIView):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', email='testemail', password='testpassword')
+
+    def test_login_success(self):
+        url = reverse('login')
+        data = {'email': 'test@xamle.com', 'password': 'passsword'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('Access_Token', response.cookies)
+
+    def test_login_failed(self):
+        url = reverse('login')
+        data = {'email': 'test@example.com', 'password': 'invalid Password'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('Access_Token', response.cookies)
+
+class LogoutViewTestCase(APITestCase):
+    def test_logout_sucess(self):
+        url = reverse('logout')
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        self.assertNotIn('Acess_Token', response.cookies)
 
