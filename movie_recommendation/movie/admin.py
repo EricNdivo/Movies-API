5588437cb50345f5822208dd8152e43d4bd1a98a
EricNdivo <j.ericndivo@gmail.com>
2024-06-05 16:51:52 +0300

    admin.py(modelregistrations)

--- movie_recommendation/movie/admin.py
+++ movie_recommendation/movie/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Movie, MovieDuration, Rating, MovieType, Follow
 
 admin.site.register(Profile)
+admin.site.register(Movie)
+admin.site.register(MovieDuration)
+admin.site.register(Rating)
+admin.site.register(MovieType)
+admin.site.register(Follow)
