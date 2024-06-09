e78f864c59742b2d78f5577c4afd4acfceda0e13
EricNdivo <j.ericndivo@gmail.com>
2024-06-09 13:23:10 +0300

    sunday

--- movie_recommendation/movie/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Movie, MovieDuration, Rating, MovieType, Follow
-
-admin.site.register(Profile)
-admin.site.register(Movie)
-admin.site.register(MovieDuration)
-admin.site.register(Rating)
-admin.site.register(MovieType)
-admin.site.register(Follow)
